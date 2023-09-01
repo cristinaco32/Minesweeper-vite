3ff1d88c16577474290ce9c4f2dc29cdd79994b8
elna <elna.ballesta.7e6@itb.cat>
2023-09-01 10:02:03 +0200

    install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/components/*.{html,jsx,js,ts,tsx}", "./src/App.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
